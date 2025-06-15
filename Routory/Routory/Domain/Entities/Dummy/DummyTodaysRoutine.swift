2c9a6490b8f67d4ea4aaeac9a74718447b108a5a
SongKyuSeob <sks5075@naver.com>
2025-06-16 03:39:04 +0900

    feat: 임시 데이터 엔티티 정의

---
+++ Routory/Routory/Domain/Entities/Dummy/DummyTodaysRoutine.swift
@@ -0,0 +1,13 @@
+//
+//  DummyRoutine.swift
+//  Routory
+//
+//  Created by 송규섭 on 6/16/25.
+//
+
+import Foundation
+
+struct DummyTodaysRoutine {
+    let workplaceName: String
+    let routines: [Routine]
+}
